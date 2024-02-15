2b6307b7014a4510b848f6470ef2e17d073476ed
Milton Denis Castro <159483393+MiltonCastro93@users.noreply.github.com>
2024-02-15 15:25:29 -0300

    Update eventListener.js

--- js/eventListener.js
+++ js/eventListener.js
@@ -1,27 +1,22 @@
-//Ubicar elementos dentro del DOM
 const citas = document.querySelector('#citas');
-/*
-const botones = document.querySelectorAll('#contenedor nav a');
-botones[0]...*/
 
 const boton = document.querySelector('#boton');
 
-//objeto.addEventListener( 'evento', accion );
-boton.addEventListener(//por cada eventos se tiene que hacer diferentes addEventlistener
+boton.addEventListener(
     'click',
-    function(){//funcion anonima no tiene nombre
+    function(){
         citas.innerText = 'hiciste click!';
     }
 );
 boton.addEventListener(
     'mouseover',
-    function(){//funcion anonima no tiene nombre
+    function(){
         citas.innerText = 'Mouse Sobre!';
     }
 );
 boton.addEventListener(
     'mouseout',
-    function(){//funcion anonima no tiene nombre
+    function(){
         citas.innerText = 'Mouse Afuera!';
     }
 );
